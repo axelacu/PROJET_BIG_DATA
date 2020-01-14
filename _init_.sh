d38b2d0fe3951d0a76bc4c0dd46754e943507201
user87 <user87@vmhadoopmaster.cluster.lamsade.dauphine.fr>
2020-01-14 23:10:42 +0100

    changing jobs

--- _init_.sh
+++ _init_.sh
@@ -1,2 +1,4 @@
-export HOME_CLUST=/home/cluster/user87/projet-bd
-export HOME_DFS=/user/user87/projet-bd
+export HOME_CLUST="/home/cluster/user87/projet-bd"
+export HOME_DFS="/user/user87/projet-bd"
+export PATH="/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games:/home/cluster/shared/vms/hadoop-current/bin:/home/cluster/shared/vms/hadoop-current/sbin:/home/cluster/shared/vms/spark-current/bin:/home/cluster/shared/vms/apache-hive-2.3.3-bin/bin"
+
--- jobs/test.sh
+++ jobs/test.sh
@@ -1,9 +1,10 @@
-start=$(date +%s.%N)
+start=$(date +%s)
 ls -al
 ls -al
 ls -al
-end=$(date +%s.%N)
+sleep 3s
+end=$(date +%s)
 
-python -c "print('temps exec : ' + str(${end} - ${start}))"
+python -c "print('temps exec : ' + str(${end} - ${start}))" > ./logs/dump.txt
 
 
